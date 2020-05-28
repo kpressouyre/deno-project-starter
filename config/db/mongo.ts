86381dd0915373f27925f14c92e1e4369ff2861d
kevin pressouyre <kevinpressouyre@kevins-MacBook-Pro.local>
2020-05-28 10:14:15 -0400

    fix error auth

--- config/db/mongo.ts
+++ config/db/mongo.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
 const client = new MongoClient();
-const url = 'mongodb://'+Deno.env.get('MONGO_USERNAME')+':'+Deno.env.get('MONGO_PASSWORD')+'@'+Deno.env.get('MONGO_HOSTNAME')+':'+Deno.env.get('MONGO_PORT')+'/'+Deno.env.get('MONGO_DB');
+const url = 'mongodb://'+Deno.env.get('MONGO_USERNAME')+':'+Deno.env.get('MONGO_PASSWORD')+'@'+Deno.env.get('MONGO_HOSTNAME')+':'+Deno.env.get('MONGO_PORT')+'/'+Deno.env.get('MONGO_DB')+'?authSource=admin';
 client.connectWithUri(url);
 
 const db = client.database("test");
